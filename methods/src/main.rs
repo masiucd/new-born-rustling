4a71877d34207217571b386cfe315d5b65408190
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-01-08 15:58:14 +0100

    (feat): rust methods

--- methods/src/main.rs
+++ methods/src/main.rs
@@ -1,19 +1,58 @@
-
-fn double(x:u8) -> u8 {
-    x *2
+#[derive(Debug)]
+enum FruitType {
+    Round,
+    Oval,
+    Sour,
+    Sweet,
 }
 
+#[derive(Debug)]
+struct Fruit {
+    name: String,
+    f_type: FruitType,
+    color: String,
+    rating: u8,
+}
 
-fn main()  {
-    let x = 45;
-    println!("hello,{}",x);
-    println!("hello,{}",x);
+impl Fruit {
+    fn eat(self, f_type: FruitType) {
+        println!("mmm {:?} am eating typeof {:?}", self.name, f_type);
+    }
+    fn new(name: String, f_type: FruitType, color: String, rating: u8) -> Fruit {
+        Fruit {
+            name: name,
+            f_type: f_type,
+            color: color,
+            rating: rating,
+        }
+    }
+}
 
-    let res = double(x);
-    println!("{}", res);
+fn foo(f: FruitType) {
+    match f {
+        FruitType::Oval => println!("mmmm"),
+        FruitType::Round => println!("mmmm,...round"),
+        FruitType::Sour => println!("mmmm,sour"),
+        FruitType::Sweet => println!("mmmm,ssweet"),
+    }
+}
 
-    let y = x;
+fn main() {
+    let apple = Fruit {
+        name: String::from("granny smith"),
+        f_type: FruitType::Sour,
+        color: String::from("green"),
+        rating: 5,
+    };
 
-    let res2 = double(y);
-    println!("{}", res2);
+    let banana = Fruit::new(
+        String::from("banana"),
+        FruitType::Sweet,
+        String::from("yellow"),
+        5,
+    );
+
+    apple.eat(FruitType::Sour);
+    foo(FruitType::Sweet);
+    println!("{:?}", banana);
 }

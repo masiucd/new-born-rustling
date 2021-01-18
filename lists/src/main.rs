4e523137629b87bae98f5ecdc59165344c8d5fe8
Masiu <ciszekmarcell@gmail.com>
2021-01-18 13:00:31 +0100

    (feat): lists in rust

---
+++ lists/src/main.rs
@@ -0,0 +1,43 @@
+use std::vec::Vec;
+
+fn iterate_through_list(xs: &[i32]) {
+    for x in xs {
+        println!("x is {}", x);
+    }
+}
+
+fn reverse_str(s: &str) -> String {
+    let res = s.chars().rev().collect();
+    res
+}
+fn reverse_str2(s: &str) -> String {
+    let mut rev_str = String::new();
+    for byte in s.chars().rev() {
+        rev_str.push(byte);
+    }
+    rev_str
+}
+
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+    iterate_through_list(&a);
+    let name = "marcell";
+    let r_name = reverse_str(name);
+    let r_name2 = reverse_str2(name);
+    println!("r_name is {}", r_name);
+    println!("r_name2 is {}", r_name2);
+
+    let names = vec!["bob", "mike", "greg"];
+    match_name(names);
+}
+
+fn match_name(names: Vec<&str>) {
+    for name in names.iter() {
+        match name {
+            &"bob" => println!("{} is cool", name),
+            &"mike" => println!("{} like to go on holidays", name),
+            &"greg" => println!("{} is chill", name),
+            _ => println!("no match"),
+        }
+    }
+}

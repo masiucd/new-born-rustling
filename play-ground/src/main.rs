dbb42e5af53913946ff2d32108f5bfa0e392cf06
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-02-04 17:56:44 +0100

    (feat): fizzbuzz

--- play-ground/src/main.rs
+++ play-ground/src/main.rs
@@ -1,11 +1,14 @@
-fn main() {
-    let x = 0u8;
-
-    match x {
-        1 => println!("one"),
-        2 => println!("two"),
-        3 => println!("three"),
-        4 => println!("four"),
-        _ => println!("no match"),
+fn fizz_buzz(n: u32) {
+    for i in 0..n {
+        match i {
+            i if (i % 5 == 0 && i % 3 == 0) => println!("fizz buzz"),
+            i if (i % 3 == 0) => println!("fizz"),
+            i if (i % 5 == 0) => println!("buzz"),
+            _ => println!("{}", n),
+        }
     }
+}
+
+fn main() {
+    fizz_buzz(100);
 }
